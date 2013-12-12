9b7cc302cdbed74c360b6487ecf3f0bdcc517775
Julien Pilet <julien.pilet@gmail.com>
2013-12-12 05:36:53 +0100

    API improvements, optimizations and polish.
    Limit cache size
    cancel old tile queries
    get/set location functions
    onLocationChange callback

--- CanvasTilesRenderer.js
+++ CanvasTilesRenderer.js
@@ -3,25 +3,55 @@ function CanvasTilesRenderer(canvas, params) {
   this.canvas = canvas;
   
   if (!("tileSize" in this.params)) this.params.tileSize = 256;
-  if (!("url" in this.params)) this.params.url = "http://a.tile2.opencyclemap.org/transport/";
-  if (!this.params.ext) this.params.ext = ".png";
+  if (!("url" in this.params)) {
+    this.params.url = function(scale, x, y) {
+      return "http://a.tile.openstreetmap.org/" + scale + '/' + x + '/' + y + '.png';
+    };
+  }
+  
+  if (!this.params.maxNumCachedTiles) this.params.maxNumCachedTiles = 64;
+  if (!this.params.maxSimultaneousLoads) this.params.maxSimultaneousLoads = 3;
   
   this.tiles = {};
   this.numLoading = 0;
   this.loadQueue = [];
-  
-  this.inDraw = false;
+
+  // Block drawing before we are ready.  
+  this.inDraw = true;
+  this.numDraw = 0;
+  this.numCachedTiles = 0;
   
   var t = this;
-  var draw = function() { t.draw(canvas); };
+  var draw = function() {
+    if (t.inDraw) { return; }
+    if (t.params.onLocationChange) { t.params.onLocationChange(t); }
+    t.draw(canvas);
+  };
   this.pinchZoom = new PinchZoom(canvas, draw);
-  
-  var constraints = [
-    { viewer: {x:0, y:0}, world: {x:0, y:0} },
-    { viewer: {x: canvas.width, y: canvas.height}, world: {x:1, y:1}}
-  ];
-  this.pinchZoom.processConstraints(constraints);
+
+  // We are ready, let's allow drawing.  
+  this.inDraw = false;
+  if (params.initialLocation) {
+    this.setLocation(params.initialLocation);
+  } else {
+    this.setLocation({x:.5, y:.5, scale: 1});
+  }
 }
+
+CanvasTilesRenderer.prototype.getLocation = function() {
+  var left = this.pinchZoom.worldPosFromViewerPos({x: 0, y:this.canvas.height / 2});
+  var right = this.pinchZoom.worldPosFromViewerPos({x: this.canvas.width, y:this.canvas.height / 2});
+  return { x: (left.x + right.x) / 2, y: (left.y + right.y) / 2, scale: (right.x - left.x)};
+};
+
+CanvasTilesRenderer.prototype.setLocation = function(location) {
+  var canvas = this.canvas;
+  var constraints = [
+    { viewer: {x:0, y: canvas.height / 2}, world: {x:location.x - location.scale /2, y: location.y} },
+    { viewer: {x:canvas.width, y: canvas.height / 2}, world: {x:location.x + location.scale /2, y: location.y} },
+  ];
+  this.pinchZoom.processConstraints(constraints);  
+};
 
 CanvasTilesRenderer.prototype.draw = function() {
   if (this.inDraw) {
@@ -80,6 +110,10 @@ CanvasTilesRenderer.prototype.draw = function() {
   
   context.restore();
   this.inDraw = false;
+  ++this.numDraw;
+  
+  // control memory usage.
+  this.limitCacheSize();
 };
 
 CanvasTilesRenderer.prototype.renderTile = function(scale, tileX, tileY, context) {
@@ -90,8 +124,8 @@ CanvasTilesRenderer.prototype.renderTile = function(scale, tileX, tileY, context
   var bottom = (tileY + 1) * zoom;
   
   var tile = this.getTile(scale, tileX, tileY);
-  if (tile && tile.complete) {
-    context.drawImage(tile, left, top, (right - left), (bottom - top));
+  if (tile && tile.image && tile.image.complete) {
+    context.drawImage(tile.image, left, top, (right - left), (bottom - top));
   }
 };
 
@@ -103,46 +137,85 @@ CanvasTilesRenderer.prototype.getTile = function(scale, x, y) {
   var key = scale + "," + x + "," + y;
   
   if (key in this.tiles) {
-    return this.tiles[key];
+    var tile = this.tiles[key];
+    tile.lastDrawRequest = this.numDraw;
+    return tile;
   }
   
-  // TODO: free memory..
-
-  var url = this.params.url + scale + '/' + x + '/' + y + this.params.ext;
+  var url = this.params.url(scale, x, y);
   this.queueTileRequest(key, url);
   return this.tiles[key];
 };
 
 CanvasTilesRenderer.prototype.queueTileRequest = function(key, url) {
-  this.loadQueue.push({key: key, url: url});
-  this.tiles[key] = false;
+  var tile = { lastDrawRequest: this.numDraw };
+  this.loadQueue.push({key: key, url: url, tile: tile});
+  this.tiles[key] = tile;
   this.processQueue();
 };
 
 CanvasTilesRenderer.prototype.processQueue = function() {
-  if (this.numLoading >= 4 || this.loadQueue.length == 0) {
-    // Busy..
+  var queue = this.loadQueue;
+  
+  // Prioritize loading
+  if (this.numLoading < this.params.maxSimultaneousLoads && queue.length > 0) {
+    this.loadQueue.sort(function(a, b) { return a.tile.lastDrawRequest - b.tile.lastDrawRequest; });
+  }
+  while (this.numLoading < this.params.maxSimultaneousLoads && queue.length > 0) {  
+    var query = this.loadQueue.pop();
+    
+    // Check if the tile is still required.
+    if ((this.numDraw - query.tile.lastDrawRequest) < 3) {
+      this.numLoading++;
+        
+      var image = new Image();
+      image.src = query.url;
+      
+      var t = this;
+      image.onload = function() { 
+        t.numLoading--;
+        t.numCachedTiles++;
+        t.tiles[query.key].image = image;
+        t.processQueue();
+        t.draw();
+      };
+      image.onerror = function() {
+        t.numLoading--;
+        t.tiles[query.key].failed = true;
+        t.processQueue();
+        console.log('Failed to load: ' + query.url);
+      };  
+    } else {
+      // There's no need to load this tile, it is not required anymore.
+      delete this.tiles[query.key];
+    }
+  }
+};
+
+CanvasTilesRenderer.prototype.limitCacheSize = function() {
+  if (this.numCachedTiles <= this.params.maxNumCachedTiles) {
+    // The cache is small enough.
     return;
   }
+
+  // Build an array of tiles we may need to remove from cache  
+  var cache = [];
+  for (var key in this.tiles) {
+    if (this.tiles[key].image) {
+      cache.push(key);
+    }
+  }
   
-  var query = this.loadQueue.pop();
+  // Sort it: oldest request first.
+  var t = this;  
+  cache.sort(function(a,b) { return t.tiles[a].lastDrawRequest - t.tiles[b].lastDrawRequest; });
   
-  this.numLoading++;
-    
-  var tile = new Image();
-  tile.src = query.url;
-  
-  var t = this;
-  tile.onload = function() { 
-    t.numLoading--;
-    t.tiles[query.key] = tile;
-    t.processQueue();
-    t.draw();
-  };
-  tile.onerror = function() {
-    t.numLoading--;
-    t.tiles[query.key] = false;
-    t.processQueue();
-    console.log('Failed to load: ' + query.url);
-  };  
+  // Remove old tiles.
+  var numToRemove = cache.length - this.params.maxNumCachedTiles;
+  for (var i = 0; i < numToRemove; ++i) {
+    var key = cache[i];
+    delete this.tiles[key];
+    this.numCachedTiles--;
+  }
 };
+
