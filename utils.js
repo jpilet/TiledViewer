8222934ff4d492c2bddae8ebf29a4d51c98b3291
Julien Pilet <julien.pilet@gmail.com>
2013-12-13 22:09:28 +0100

    Fix bad touch coordinate bug.

--- utils.js
+++ utils.js
@@ -49,23 +49,12 @@ this.Utils = {
     return Math.sqrt(dx * dx + dy * dy);
   },
 
-  eventPosInElementCoordinates: function (event, element){
-    var totalOffsetX = 0;
-    var totalOffsetY = 0;
-    var canvasX = 0;
-    var canvasY = 0;
-    var currentElement = element;
-
-    do{
-        totalOffsetX += currentElement.offsetLeft - currentElement.scrollLeft;
-        totalOffsetY += currentElement.offsetTop - currentElement.scrollTop;
-    }
-    while(currentElement = currentElement.offsetParent);
-
-    canvasX = event.pageX - totalOffsetX;
-    canvasY = event.pageY - totalOffsetY;
-
-    return {x:canvasX, y:canvasY};
+  eventPosInElementCoordinates: function(event, element) {
+    var rect = element.getBoundingClientRect();
+    return {
+      x: event.clientX - rect.left,
+      y: event.clientY - rect.top
+    };    
   },
 };
 
