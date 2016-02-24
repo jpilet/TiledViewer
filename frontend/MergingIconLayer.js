3532479a0bb63830d4a8799d8c334dd12714cf07
Julien Pilet <julien.pilet@gmail.com>
2016-02-24 09:30:56 +0100

    Optimize with D3 quadtree

--- frontend/MergingIconLayer.js
+++ frontend/MergingIconLayer.js
@@ -38,8 +38,62 @@ function searchForHighestValue(cells) {
   return bestIndex;
 }
 
+function d3_geom_quadtreeReduce(node, reduced, f) {
+  var children = node.nodes;
+  if (children.length == 0) {
+    return f(node, [], []);
+  }
+
+  var reduced = [];
+  for (var i in children) {
+    reduced.push(d3_geom_quadtreeReduce(children[i], reduced, f));
+  }
+  return f(node, reduced, children);
+}
+
 MergingIcon.prototype.setPoints = function(points) {
   this.points = points;
+  var Quadtree = d3.geom.quadtree();
+  Quadtree.x(function(p) { return p.x; });
+  Quadtree.y(function(p) { return p.y; });
+
+  // D3 assumes that 0.01 is a distance small enough to stop recursing.
+  // working in a 0-1 range does not work. We scale to 1024 instead.
+  Quadtree.extent([[0, 0], [1024, 1024]]);
+  var scaledPoints = new Array(points.length);
+  for (var i in points) {
+    scaledPoints[i] = { x: 1024 * points[i].x, y: 1024 * points[i].y };
+  }
+  this.quadtree = Quadtree(scaledPoints);
+  this.quadtree.reduce = function(f) {
+    return d3_geom_quadtreeReduce(this, undefined, f);
+  };
+
+  this.quadtree.reduce(function(node, values, nodes) {
+    var reduced = {
+     x: 0,
+     y: 0,
+     count : 0
+    };
+    if (node.point) {
+      reduced.x = node.x;
+      reduced.y = node.y;
+      reduced.count = 1;
+    }
+    for (var i in values) {
+      var count = values[i].count;
+      if (count > 0) {
+        var total = count + reduced.count;
+        reduced.x = (reduced.x * reduced.count + values[i].x * count) / total;
+        reduced.y = (reduced.y * reduced.count + values[i].y * count) / total;
+        reduced.count = total;
+      }
+    }
+    node.reduced = reduced;
+    return reduced;
+  });
+
+  Utils.assert(this.quadtree.reduced.count == points.length);
 };
 
 MergingIcon.prototype.draw = function(canvas, pinchZoom,
@@ -180,20 +234,6 @@ MergingIcon.prototype.draw = function(canvas, pinchZoom,
 
     this.drawMergedIcon(p, canvas, pinchZoom);
     clearWindow(win);
-    
-    /*
-    clearWindow(
-        { x: Math.max(0, cellX - 2), y : Math.max(0, cellY - 2) },
-        { x: Math.min(numCellsX - 1, cellX + 2), y : Math.min(numCellsY - 1, cellY + 2) });
-    */
-
-    /*
-    var c = worldToCell(p.x, p.y);
-    Utils.assert(c.x == cellX);
-    Utils.assert(c.y == cellY);
-    clearWindow(worldToCell(p.x - rad, p.y - rad),
-                worldToCell(p.x + rad, p.y + rad))
-                */
   }
 }
 
@@ -210,6 +250,60 @@ function isInside(p, topLeft, bottomRight) {
 }
 
 MergingIcon.prototype.statsForWindow = function(topLeft, bottomRight) {
+  var result = { x:0, y:0, count: 0 };
+  Utils.assert(topLeft.x <= bottomRight.x);
+  Utils.assert(topLeft.y <= bottomRight.y);
+
+  var minX = 1024 * topLeft.x;
+  var maxX = 1024 * bottomRight.x;
+  var minY = 1024 * topLeft.y;
+  var maxY = 1024 * bottomRight.y;
+  this.quadtree.visit(function(node, x1, y1, x2, y2) {
+    if (x1 > maxX || x2 < minX
+        || y1 > maxY || y2 < minY) {
+      // Fully outside. stop recursion, count nothing.
+      return 1;
+    }
+    if (x1 >= minX && x2 <= maxX
+        && y1 >= minY && y2 <= maxY) {
+      // Fully inside. We count the whole subtree without recursing.
+      var p = { x: node.reduced.x / 1024, y: node.reduced.y / 1024 };
+      result.x += p.x * node.reduced.count;
+      result.y += p.y * node.reduced.count;
+      result.count += node.reduced.count;
+      return 1;
+    }
+
+    if (node.point) {
+      var p = { x: node.x / 1024, y: node.y / 1024 };
+      if (isInside(p, topLeft, bottomRight)) {
+          result.x += p.x;
+          result.y += p.y;
+          result.count++;
+      }
+      return 0;
+    }
+  });
+
+  if (result.count) {
+    result.x /= result.count;
+    result.y /= result.count;
+  }
+
+  if (false) {
+    var ref = this.statsForWindowRef(topLeft, bottomRight);
+    //Utils.assert(ref.count == result.count);
+    if (ref.count != result.count) {
+      console.log('Ref: ' + ref.count + ' quadtree: ' + result.count);
+    }
+    if (ref.count > 0 && (ref.x != result.x || ref.y != result.y)) {
+      console.log("ref: " + ref.x + "," + ref.y + "  result: " + result.x + "," + result.y);
+    }
+  }
+  return result;
+}
+
+MergingIcon.prototype.statsForWindowRef = function(topLeft, bottomRight) {
   var sum = { x: 0, y: 0 };
   var count = 0;
   for (var i = 0; i < this.points.length; ++i) {
