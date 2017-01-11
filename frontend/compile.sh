60ac18e5355a80de08c40eee1eacd8fd9f74f87f
Julien Pilet <julien.pilet@gmail.com>
2017-01-11 11:40:53 +0100

    compile mapviewer.js from npm

--- frontend/compile.sh
+++ frontend/compile.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+cd "$( dirname "${BASH_SOURCE[0]}" )"
+
 FILES="utils.js affinetransform.js pinchzoom.js CanvasTilesRenderer.js htmlinterface.js TileLayer.js ScaleLayer.js"
 
-cat $FILES | jsmin > mapviewer.min.js
-cat $FILES > mapviewer.js
+cat $FILES | jsmin > ../mapviewer.min.js
+cat $FILES > ../mapviewer.js
