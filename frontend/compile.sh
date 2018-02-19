bb4024304240885b1fbe4260b370c68354ba658f
Ivan Gonzales <i.gonzales@arcanys.com>
2018-02-19 13:43:42 +0800

    added it back

--- frontend/compile.sh
+++ frontend/compile.sh
@@ -5,5 +5,5 @@ cd "$( dirname "${BASH_SOURCE[0]}" )"
 FILES="utils.js affinetransform.js pinchzoom.js CanvasTilesRenderer.js htmlinterface.js TileLayer.js ScaleLayer.js 
 POILayer.js ArrowLayer.js"
 
-cat $FILES | jsmin > ../build/mapviewer.min.js
-cat $FILES > ../build/mapviewer.js
+cat $FILES | jsmin > ../mapviewer.min.js
+cat $FILES > ../mapviewer.js
